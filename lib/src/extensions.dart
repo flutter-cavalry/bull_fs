a32b4d4fb1903935ef0bdd0e3aed63bb5a5a916f
Mgen <m@mgenware.com>
2024-10-05 21:37:19 +0800

    Remove some extension funcs

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -3,19 +3,8 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'bf_env.dart';
 import 'types.dart';
 
-extension IListStringExtension on IList<String> {
-  IList<String> parentDir() {
-    if (length == 0) {
-      throw Exception('Cannot get parent dir of a root dir');
-    }
-    if (length == 1) {
-      return IList<String>();
-    }
-    return take(length - 1).toIList();
-  }
-}
-
 extension BFEnvExtension on BFEnv {
+  /// Deletes a path if it exists.
   Future<void> deletePathIfExists(
       BFPath path, IList<String>? extendedPath) async {
     final pathInfo = await itemExists(path, extendedPath);
@@ -23,18 +12,6 @@ extension BFEnvExtension on BFEnv {
       return;
     }
     await delete(pathInfo.path, pathInfo.isDir);
-  }
-
-  Future<BFPath> ensureDirsForFile(
-      BFPath dir, IList<String> relFilePath) async {
-    if (relFilePath.length == 1) {
-      final dirStat = await stat(dir);
-      if (dirStat == null) {
-        throw Exception('Parent dir does not exist: $dir');
-      }
-      return dir;
-    }
-    return mkdirp(dir, relFilePath.take(relFilePath.length - 1).toIList());
   }
 
   Future<Map<String, dynamic>> directoryToMap(BFPath dir,
