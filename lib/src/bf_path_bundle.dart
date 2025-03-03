2095d2c5f8d8a1a70c79be9300c12c2725b01b34
Mgen <m@mgenware.com>
2025-03-03 15:59:49 +0800

    Add BFPathNameBundle

--- lib/src/bf_path_bundle.dart
+++ lib/src/bf_path_bundle.dart
@@ -16,3 +16,23 @@ class BFPathBundle {
       : env = BFEnvUtil.typeToEnv(json['env'] as BFEnvType),
         path = decodeStringToBFPath(json['path'] as String);
 }
+
+/// Like [BFPathBundle], but with an extra [name] field.
+class BFPathNameBundle {
+  final BFEnv env;
+  final BFPath path;
+  final String name;
+
+  BFPathNameBundle(this.env, this.path, this.name);
+
+  Map<String, dynamic> toJson() => {
+        'env': env.envType(),
+        'path': path.encodeToString(),
+        'name': name,
+      };
+
+  BFPathNameBundle.fromJson(Map<String, dynamic> json)
+      : env = BFEnvUtil.typeToEnv(json['env'] as BFEnvType),
+        path = decodeStringToBFPath(json['path'] as String),
+        name = json['name'] as String;
+}
