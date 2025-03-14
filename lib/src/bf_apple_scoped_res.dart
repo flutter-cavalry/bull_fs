db866982dbb704031cb8d900a788600253ff75a9
Mgen <m@mgenware.com>
2025-03-14 17:38:43 +0800

    Only expose `tryAccess` in `BFAppleScopedRes`

--- lib/src/bf_apple_scoped_res.dart
+++ lib/src/bf_apple_scoped_res.dart
@@ -13,20 +13,8 @@ class BFAppleScopedRes {
     _res = AppleScopedResource(url, isFilePath: isFilePath);
   }
 
-  /// Request access to the security scoped resource.
-  @Deprecated('Use [useAccess] instead.')
-  Future<void> requestAccess() async {
-    await _res.requestAccess();
-  }
-
-  /// Release the access to the security scoped resource.
-  @Deprecated('Use [useAccess] instead.')
-  Future<void> release() async {
-    await _res.release();
-  }
-
   /// Request access to the security scoped resource and run the action. This also releases the access after the action is done.
-  Future<bool> useAccess(Future<void> Function() action) async {
-    return _res.useAccess(action);
+  Future<void> tryAccess(Future<void> Function(bool granted) action) async {
+    await _res.tryAccess(action);
   }
 }
